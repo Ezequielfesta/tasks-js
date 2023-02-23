f96874d482218fce6bb66c4a0184b2a684ae2998
Ezequielfesta <ezequiel.festa@gmail.com>
2023-02-23 00:25:44 -0300

    datos dinamicos js home, up, past

---
+++ scripts/past_events.js
@@ -0,0 +1,46 @@
+//CARD CONTAINER 
+
+for (i = 0; i < data.events.length; i++) {
+
+  if(Date.parse(data.currentDate) > Date.parse(data.events[i].date)) {
+    //console.log("El evento "+data.events[i]._id+" es Past Event")
+  let cardcontainerElement = document.getElementById("cardcontainer");
+  let cardElement = document.createElement("div");
+  cardElement.className = "card bg-dark text-white";
+  let cardimgElement = document.createElement("img");
+  cardimgElement.src = data.events[i].image;
+  cardimgElement.alt = data.events[i].category;
+  cardimgElement.className = "card-img-top";
+  let cardbodyElement = document.createElement("div");
+  cardbodyElement.className = "card-body";
+  let cardtitleElement = document.createElement("h5");
+  cardtitleElement.className = "card-title";
+  cardtitleElement.innerHTML = data.events[i].name;
+  let cardtextElement = document.createElement("p");
+  cardtextElement.className = "card-text";
+  cardtextElement.innerHTML = data.events[i].description;
+  let cardrowElement = document.createElement("div");
+  cardrowElement.className = "row mt-4";
+  let cardcol1Element = document.createElement("div");
+  cardcol1Element.className = "col colcenter";
+  let cardcol2Element = document.createElement("div");
+  cardcol2Element.className = "col colcenter";
+  let cardpriceElement = document.createElement("p");
+  cardpriceElement.innerHTML = "Price:<br>$ " + data.events[i].price;
+  let cardanchorElement = document.createElement("a");
+  cardanchorElement.className = "btn btn-dark crazyborder";
+  cardanchorElement.href = "./details.html#Cinema";
+  cardanchorElement.innerHTML = "Details";
+
+  cardcontainerElement.appendChild(cardElement);
+  cardElement.appendChild(cardimgElement);
+  cardElement.appendChild(cardbodyElement);
+  cardbodyElement.appendChild(cardtitleElement);
+  cardbodyElement.appendChild(cardtextElement);
+  cardbodyElement.appendChild(cardrowElement);
+  cardrowElement.appendChild(cardcol1Element);
+  cardrowElement.appendChild(cardcol2Element);
+  cardcol1Element.appendChild(cardpriceElement);
+  cardcol2Element.appendChild(cardanchorElement);
+}
+};
