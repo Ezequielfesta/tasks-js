1c2fc678300622efb2efa37ff3a0a88a990c91cb
Ezequielfesta <ezequiel.festa@gmail.com>
2023-03-06 03:53:00 -0300

    checkbox filter

--- scripts/past_events.js
+++ scripts/past_events.js
@@ -1,70 +1,94 @@
-//CHECKBOX CONTAINER
-
+//DYNAMIC CHECKBOX GENERATOR
+//--------------------------
 let categories = [];
 for (i = 0; i < data.events.length; i++) {
-if(!categories.includes(data.events[i].category))
-{categories.push(data.events[i].category)}
+  if (!categories.includes(data.events[i].category)) { categories.push(data.events[i].category) }
 }
 
 for (i = 0; i < categories.length; i++) {
-let formcheckinlineElement = document.getElementById("checkboxcontainer");
-formcheckinlineElement.className = "form-check-inline";
-let formcheckinputElement = document.createElement("input");
-formcheckinputElement.className = "form-check-input";
-formcheckinputElement.type = "checkbox";
-formcheckinputElement.value = "category"+(i+1);
-formcheckinputElement.id = "inlineCheckbox"+(i+1);
-let formchecklabelElement = document.createElement("label");
-formchecklabelElement.for = "inlineCheckbox"+(i+1);
-formchecklabelElement.innerHTML = categories[i];
-
-formcheckinlineElement.appendChild(formcheckinputElement);
-formcheckinlineElement.appendChild(formchecklabelElement);
+  let formcheckinlineElement = document.getElementById("checkboxcontainer");
+  formcheckinlineElement.className = "form-check-inline";
+  let formcheckdivElement = document.createElement("div");
+  formcheckdivElement.className = "checkboxgroup";
+  let formcheckinputElement = document.createElement("input");
+  formcheckinputElement.className = "form-check-input";
+  formcheckinputElement.type = "checkbox";
+  formcheckinputElement.value = categories[i];
+  formcheckinputElement.id = "checkbox" + i;
+  formcheckinputElement.checked = "checked";
+  let formchecklabelElement = document.createElement("label");
+  formchecklabelElement.for = "checkbox" + i;
+  formchecklabelElement.innerHTML = categories[i];
+  formcheckinlineElement.appendChild(formcheckdivElement);
+  formcheckdivElement.appendChild(formcheckinputElement);
+  formcheckdivElement.appendChild(formchecklabelElement);
 }
 
-//CARD CONTAINER 
+let checkboxButtons = document.querySelectorAll("input[type=checkbox]");
+checkboxButtons.forEach(checkbox => checkbox.addEventListener("change", isSelected));
+let selected = Array.from(checkboxButtons).filter(checkbox => checkbox.checked);
+generateCards(selected);
 
-for (i = 0; i < data.events.length; i++) {
+function isSelected() {
+  let selected = Array.from(checkboxButtons).filter(checkbox => checkbox.checked)
+  generateCards(selected)
+}
 
-  if(Date.parse(data.currentDate) > Date.parse(data.events[i].date)) {
-    //console.log("El evento "+data.events[i]._id+" es Past Event")
+
+//DYNAMIC CARD GENERATOR 
+//----------------------
+function generateCards(param) {
   let cardcontainerElement = document.getElementById("cardcontainer");
-  let cardElement = document.createElement("div");
-  cardElement.className = "card bg-dark text-white";
-  let cardimgElement = document.createElement("img");
-  cardimgElement.src = data.events[i].image;
-  cardimgElement.alt = data.events[i].category;
-  cardimgElement.className = "row card-img-top";
-  let cardbodyElement = document.createElement("div");
-  cardbodyElement.className = "row card-body";
-  let cardtitleElement = document.createElement("h5");
-  cardtitleElement.className = "row card-title";
-  cardtitleElement.innerHTML = data.events[i].name;
-  let cardtextElement = document.createElement("p");
-  cardtextElement.className = "row card-text";
-  cardtextElement.innerHTML = data.events[i].description;
-  let cardrowElement = document.createElement("div");
-  cardrowElement.className = "row mt-4";
-  let cardcol1Element = document.createElement("div");
-  cardcol1Element.className = "col cardcolcenter";
-  let cardcol2Element = document.createElement("div");
-  cardcol2Element.className = "col cardcolcenter right";
-  let cardpriceElement = document.createElement("p");
-  cardpriceElement.innerHTML = "Price:<br>$ " + data.events[i].price;
-  let cardanchorElement = document.createElement("a");
-  cardanchorElement.className = "btn btn-dark crazyborder";
-  cardanchorElement.href = "./details.html?id=" + data.events[i]._id;
-  cardanchorElement.innerHTML = "Details";
+  cardcontainerElement.innerHTML = "";
+  let cardExists = false;
+  for (i = 0; i < data.events.length; i++) {
+    if(Date.parse(data.currentDate) > Date.parse(data.events[i].date)) {
+    for (j = 0; j < param.length; j++) {
+      if (param[j].value == data.events[i].category) {
+        cardExists = true;
+        let cardElement = document.createElement("div");
+        cardElement.className = "card bg-dark text-white";
+        let cardimgElement = document.createElement("img");
+        cardimgElement.src = data.events[i].image;
+        cardimgElement.alt = data.events[i].category;
+        cardimgElement.className = "row card-img-top";
+        let cardbodyElement = document.createElement("div");
+        cardbodyElement.className = "row card-body";
+        let cardtitleElement = document.createElement("h5");
+        cardtitleElement.className = "row card-title";
+        cardtitleElement.innerHTML = data.events[i].name;
+        let cardtextElement = document.createElement("p");
+        cardtextElement.className = "row card-text";
+        cardtextElement.innerHTML = data.events[i].description;
+        let cardrowElement = document.createElement("div");
+        cardrowElement.className = "row mt-4";
+        let cardcol1Element = document.createElement("div");
+        cardcol1Element.className = "col cardcolcenter";
+        let cardcol2Element = document.createElement("div");
+        cardcol2Element.className = "col cardcolcenter right";
+        let cardpriceElement = document.createElement("p");
+        cardpriceElement.innerHTML = "Price:<br>$ " + data.events[i].price;
+        let cardanchorElement = document.createElement("a");
+        cardanchorElement.className = "btn btn-dark crazyborder";
+        cardanchorElement.href = "./details.html?id=" + data.events[i]._id;
+        cardanchorElement.innerHTML = "Details";
 
-  cardcontainerElement.appendChild(cardElement);
-  cardElement.appendChild(cardimgElement);
-  cardElement.appendChild(cardbodyElement);
-  cardbodyElement.appendChild(cardtitleElement);
-  cardbodyElement.appendChild(cardtextElement);
-  cardbodyElement.appendChild(cardrowElement);
-  cardrowElement.appendChild(cardcol1Element);
-  cardrowElement.appendChild(cardcol2Element);
-  cardcol1Element.appendChild(cardpriceElement);
-  cardcol2Element.appendChild(cardanchorElement);
+        cardcontainerElement.appendChild(cardElement);
+        cardElement.appendChild(cardimgElement);
+        cardElement.appendChild(cardbodyElement);
+        cardbodyElement.appendChild(cardtitleElement);
+        cardbodyElement.appendChild(cardtextElement);
+        cardbodyElement.appendChild(cardrowElement);
+        cardrowElement.appendChild(cardcol1Element);
+        cardrowElement.appendChild(cardcol2Element);
+        cardcol1Element.appendChild(cardpriceElement);
+        cardcol2Element.appendChild(cardanchorElement);
+      }
+      }
+    }
+  }
+
+  if (cardExists === false) {
+    cardcontainerElement.innerHTML = "<h3 class=text-white>No matching results.</h3><h5 class=text-white>Try selecting a different filter.</h5>";
+  }
 }
-};
